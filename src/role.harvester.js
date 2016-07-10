782bda735beb5f477a57ef81db9af5c2d3e82a44
Snodig <snodig@gmail.com>
2016-07-10 19:30:08 +0200

    Made worker role rely on harvester role for mining behaviour

--- src/role.harvester.js
+++ src/role.harvester.js
@@ -1,98 +1,118 @@
 /*
- * Module code goes here. Use 'module.exports' to export things:
- * module.exports.thing = 'a thing';
- *
- * You can import it from another modules like this:
- * var mod = require('role.harvester');
- * mod.thing == 'a thing'; // true
+ * Harvester role and functions
  */
 
-var roleHarvester = {
-
-    /** @param {Creep} creep **/
-    run: function(creep) {
-        creep.say('lol');
-        if(creep.carry.energy < creep.carryCapacity)
-        {
-            creep.memory.currentSource = 0;
-            var sources = creep.room.find(FIND_SOURCES);
-            for(i = 0; i < sources.length; ++i)
-            {
-                let targetPath = creep.pos.findPathTo(sources[i]);
-                let targetPos = creep.room.getPositionAt(targetPath[targetPath.length-1].x, targetPath[targetPath.length-1].y);
-                if(targetPos.inRangeTo(sources[i].pos, 1))
-                {
-                    creep.memory.currentSource = sources[i].id;
-                    break;
-                }
-                else
-                {
-                    continue;
-                }
-            }
-
-            if(creep.memory.currentSource == 0)
-            {
-                var closest = creep.pos.findClosestByRange(FIND_SOURCES);
-                if(creep.pos.getRangeTo(closest) > 3)
-                {
-                    creep.moveTo(closest);
-                }
-                else
-                {
-                    var spawn = creep.pos.findClosestByRange(FIND_MY_SPAWNS)
-                    creep.moveTo(spawn);
-                }
-            }
-            else
-            {
-                // Mine current source
-                var source = Game.getObjectById(creep.memory.currentSource);
-                console.log(creep.name + " using saved source: " + creep.memory.currentSource)
-
-                let targetPath = creep.pos.findPathTo(source);
-                let targetPos = creep.room.getPositionAt(targetPath[targetPath.length-1].x, targetPath[targetPath.length-1].y);
-                if(targetPos.inRangeTo(source.pos, 1))
-                {
-                    let err = creep.moveTo(source);
-                    creep.harvest(source);
-                    if(err != OK)
-                    {
-                        creep.say(err);
-                    }
-                }
-                else
-                {
-                    creep.say("No path");
-                    creep.memory.currentSource = 0;
-                }
-            }
-        }
-        else // Reached capacity
-        {
-            var targets = creep.room.find(FIND_STRUCTURES, {
-                    filter: (structure) => {
-                        return (structure.structureType == STRUCTURE_EXTENSION ||
-                                structure.structureType == STRUCTURE_SPAWN ||
-                                structure.structureType == STRUCTURE_TOWER) && structure.energy < structure.energyCapacity;
-                    }
-            });
-
-            if(targets.length > 0)
-            {
-                if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE)
-                {
-                    creep.moveTo(targets[0]);
-                }
-            }
-            else
-            {
-                creep.say("Idle")
-                var spawn = creep.pos.findClosestByRange(FIND_MY_SPAWNS)
-                creep.moveTo(spawn);
-            }
-        }
-    }
+module.exports = {
+    run : run,
+    deposit:deposit,
+    findSource:findSource,
+    mineSource:mineSource
 };
 
-module.exports = roleHarvester;
+/** @param {Creep} creep **/
+function findSource(creep)
+{
+    var sources = creep.room.find(FIND_SOURCES);
+    for(i = 0; i < sources.length; ++i)
+    {
+        let targetPath = creep.pos.findPathTo(sources[i]);
+        let targetPos = creep.room.getPositionAt(targetPath[targetPath.length-1].x, targetPath[targetPath.length-1].y);
+        if(targetPos.inRangeTo(sources[i].pos, 1))
+        {
+            return creep.memory.currentSource = sources[i].id;
+        }
+        else
+        {
+            continue;
+        }
+    }
+
+    return 0;
+}
+
+/** @param {Creep} creep **/
+function mineSource(creep)
+{
+    if(creep.memory.currentSource != 0)
+    {
+        // Mine current source
+        var source = Game.getObjectById(creep.memory.currentSource);
+        console.log(creep.name + " using saved source: " + creep.memory.currentSource)
+
+        let targetPath = creep.pos.findPathTo(source);
+        let targetPos = creep.room.getPositionAt(targetPath[targetPath.length-1].x, targetPath[targetPath.length-1].y);
+        if(targetPos.inRangeTo(source.pos, 1))
+        {
+            let err = creep.moveTo(source);
+            creep.harvest(source);
+            if(err != OK)
+            {
+                creep.say(err);
+            }
+        }
+        else
+        {
+            creep.say("No path");
+            creep.memory.currentSource = 0;
+            return false;
+        }
+
+        return true;
+    }
+    else
+    {
+        // No available source found, hover around the closest source
+        var closest = creep.pos.findClosestByRange(FIND_SOURCES);
+        if(creep.pos.getRangeTo(closest) > 3)
+        {
+            creep.moveTo(closest);
+        }
+        else
+        {
+            var spawn = creep.pos.findClosestByRange(FIND_MY_SPAWNS)
+            creep.moveTo(spawn);
+        }
+
+        return false;
+    }
+}
+
+/** @param {Creep} creep **/
+function deposit(creep)
+{
+    var targets = creep.room.find(FIND_STRUCTURES, {
+            filter: (structure) => {
+                return (structure.structureType == STRUCTURE_EXTENSION ||
+                        structure.structureType == STRUCTURE_SPAWN ||
+                        structure.structureType == STRUCTURE_TOWER) && structure.energy < structure.energyCapacity;
+            }
+    });
+
+    if(targets.length > 0)
+    {
+        if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE)
+        {
+            creep.moveTo(targets[0]);
+        }
+    }
+    else
+    {
+        creep.say("Idle")
+        var spawn = creep.pos.findClosestByRange(FIND_MY_SPAWNS)
+        creep.moveTo(spawn);
+    }
+}
+
+/** @param {Creep} creep **/
+function run(creep)
+{
+    if(creep.carry.energy < creep.carryCapacity)
+    {
+        creep.memory.currentSource = findSource(creep);
+        mineSource(creep);
+    }
+    else // Reached capacity
+    {
+        deposit(creep);
+    }
+}
--- src/role.worker.js
+++ src/role.worker.js
@@ -1,109 +1,65 @@
 /*
- * Module code goes here. Use 'module.exports' to export things:
- * module.exports.thing = 'a thing';
- *
- * You can import it from another modules like this:
- * var mod = require('role.worker');
- * mod.thing == 'a thing'; // true
+ * Worker role
  */
 
-var roleWorker = {
+module.exports = {
+    run : run
+};
 
-    /** @param {Creep} creep **/
-    run: function(creep) {
+var roleHarvester = require('role.harvester');
 
-        if(creep.memory.working && creep.carry.energy == 0) {
-            creep.memory.working = false;
-        }
+function run(creep)
+{
+    if(creep.memory.working && creep.carry.energy == 0)
+    {
+        creep.memory.working = false;
+    }
 
-        if(!creep.memory.working && creep.carry.energy == creep.carryCapacity) {
-            creep.memory.working = true;
-        }
+    if(!creep.memory.working && creep.carry.energy == creep.carryCapacity)
+    {
+        creep.memory.working = true;
+    }
 
-        if(creep.memory.working)
+    if(creep.memory.working)
+    {
+        var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
+        if(targets.length)
         {
-            var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
-            if(targets.length)
+            if(creep.build(targets[0]) == ERR_NOT_IN_RANGE)
             {
-                if(creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(targets[0]);
-                }
-                return;
+                creep.moveTo(targets[0]);
             }
-
-            if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(creep.room.controller);
-                return;
-            }
-
-            var closestDamagedStructure = creep.pos.findClosestByRange(FIND_STRUCTURES, {
-                filter: (structure) => structure.hits < structure.hitsMax
-            });
-            if(closestDamagedStructure) {
-                creep.moveTo(closestDamagedStructure);
-                creep.repair(closestDamagedStructure);
-                return;
-            }
-
             return;
         }
-        // Todo: Define a controller that can be asked to return a source with a free spot to mine
-        else {
-            creep.memory.currentSource = 0;
-            var sources = creep.room.find(FIND_SOURCES);
-            for(i = 0; i < sources.length; ++i)
-            {
-                let targetPath = creep.pos.findPathTo(sources[i]);
-                let targetPos = creep.room.getPositionAt(targetPath[targetPath.length-1].x, targetPath[targetPath.length-1].y);
-                if(targetPos.inRangeTo(sources[i].pos, 1))
-                {
-                    creep.memory.currentSource = sources[i].id;
-                    break;
-                }
-                else
-                {
-                    continue;
-                }
-            }
 
-            if(creep.memory.currentSource == 0)
-            {
-                var closest = creep.pos.findClosestByRange(FIND_SOURCES);
-                if(creep.pos.getRangeTo(closest) > 3)
-                {
-                    creep.moveTo(closest);
-                }
-                else
-                {
-                    var spawn = creep.pos.findClosestByRange(FIND_MY_SPAWNS)
-                    creep.moveTo(spawn);
-                }
-            }
-            else
-            {
-                // Mine current source
-                var source = Game.getObjectById(creep.memory.currentSource);
-                console.log(creep.name + " using saved source: " + creep.memory.currentSource)
+        if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE)
+        {
+            creep.moveTo(creep.room.controller);
+            return;
+        }
 
-                let targetPath = creep.pos.findPathTo(source);
-                let targetPos = creep.room.getPositionAt(targetPath[targetPath.length-1].x, targetPath[targetPath.length-1].y);
-                if(targetPos.inRangeTo(source.pos, 1))
-                {
-                    let err = creep.moveTo(source);
-                    creep.harvest(source);
-                    if(err != OK)
-                    {
-                        creep.say(err);
-                    }
-                }
-                else
-                {
-                    creep.say("No path");
-                    creep.memory.currentSource = 0;
-                }
+        var closestDamagedStructure = creep.pos.findClosestByRange(FIND_STRUCTURES, {
+            filter: (structure) => structure.hits < structure.hitsMax
+        });
+
+        if(closestDamagedStructure)
+        {
+            creep.moveTo(closestDamagedStructure);
+            creep.repair(closestDamagedStructure);
+            return;
+        }
+
+        return;
+    }
+    else
+    {
+        creep.memory.currentSource = roleHarvester.findSource(creep);
+        if(roleHarvester.mineSource(creep) == false)
+        {
+            if(creep.carry.energy > 0)
+            {
+                creep.memory.working = true;
             }
         }
     }
-};
-
-module.exports = roleWorker;
+}
